dea002ec61e613226dc4d93682e6a49d03b4f0b8
anhivan <ivan.isakov@hotmail.com>
2021-08-14 13:40:31 +0200

    Created Sugue
    Created a segue to the questionnaire view controller. Now the fun begins!

--- what-to-eat/Controller/IntroductionViewController.swift
+++ what-to-eat/Controller/IntroductionViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class IntroductionViewController: UIViewController {
 
+    @IBOutlet weak var beginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func beginButtonPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goToQuestionaire", sender: self)
+    }
+    
+    //Initialise segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //Check which segue has been chosen, in this case ResultViewController
+        if segue.identifier == "goToQuestionaire" {
+            //Create a variable holding reference to ResultViewController so we can access it's variables & methods
+            let destinationVC = segue.destination as! QuestionaireViewController
+            //Access it's bmiValue variable
+        }
+    }
+    
+    
 }
 
---
+++ what-to-eat/Controller/QuestionaireViewController.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionaireViewController.swift
+//  what-to-eat
+//
+//  Created by Ivan Isakov on 14/08/2021.
+//
+
+import Foundation
+import UIKit
+
+class QuestionaireViewController: UIViewController {
+    
+}
